acc2cea4fa5af3cd548112fbfd94528ec02d89ec
franzy111 <ksosnovsky21@mail.ru>
2023-10-09 23:52:15 +0500

    add JavaDoc for bot interface

--- src/main/java/org/opp/core/Bot.java
+++ src/main/java/org/opp/core/Bot.java
@@ -1,5 +1,13 @@
 package org.opp.core;
 
+/**
+ * Интерфейс бота
+ */
 public interface Bot {
+    /**
+     * Метод для отправки сообщений
+     * @param id
+     * @param message
+     */
     public void sendMessage(Long id, String message);
 }
