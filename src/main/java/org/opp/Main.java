ccb657dcdaaac081c81b8008b2f83f80ce4817d5
franzy111 <ksosnovsky21@mail.ru>
2023-12-04 18:00:50 +0500

    Add all files for VK integration

--- src/main/java/org/opp/Main.java
+++ src/main/java/org/opp/Main.java
@@ -1,6 +1,8 @@
 package org.opp;
 
+import api.longpoll.bots.exceptions.VkApiException;
 import org.opp.bots.TelegramBot;
+import org.opp.bots.VKBot;
 import org.opp.core.ManagerResponse;
 import org.opp.utils.Config;
 
@@ -8,10 +10,11 @@ import org.opp.utils.Config;
  *Класс Main
  */
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws VkApiException {
         Config cfg = new Config();
 
         ManagerResponse managerResponse = new ManagerResponse();
         TelegramBot.launch(cfg.getName(), cfg.getTokenTG(), managerResponse);
+        new VKBot(cfg.getTokenVK(), managerResponse).startPolling();
     }
 }
--- src/main/java/org/opp/utils/Config.java
+++ src/main/java/org/opp/utils/Config.java
@@ -3,6 +3,7 @@ package org.opp.utils;
 public class Config {
     public String getName() {return "";}
     public String getTokenTG() {return "";}
+    public String getTokenVK() {return "";}
     public String getDBhost(){return "";}
     public String getDBport(){return "";}
     public String getDBname(){return "";}
